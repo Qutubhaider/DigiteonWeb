d9328e4f15acb9712ea5954fb651664f77cffc70
qutubhaider950@gmail.com <qutubhaider950@gmail.com>
2022-04-17 01:41:48 +0530

    Update All Pages

--- DigiteonWeb/Controllers/HomeController.cs
+++ DigiteonWeb/Controllers/HomeController.cs
@@ -40,6 +40,48 @@ namespace DigiteonWeb.Controllers
             return View("~/Views/Home/Contact.cshtml");
         }
 
+        [Route("training-programs/scaled-agile")]
+        public IActionResult ScaledAgile()
+        {
+            return View("~/Views/Home/Scaled-Agile.cshtml");
+        }
+
+        [Route("training-programs/scrum-alliance")]
+        public IActionResult ScrumAlliance()
+        {
+            return View("~/Views/Home/Scrum-Alliance.cshtml");
+        }
+
+        [Route("training-programs/kanban")]
+        public IActionResult Kanban()
+        {
+            return View("~/Views/Home/Kanban.cshtml");
+        }
+
+        [Route("services/it-consulting-and-solutions")]
+        public IActionResult ITConsulting()
+        {
+            return View("~/Views/Home/ITConsulting.cshtml");
+        }
+
+        [Route("services/training-program")]
+        public IActionResult Training()
+        {
+            return View("~/Views/Home/TrainingProgram.cshtml");
+        }
+
+        [Route("services/recruitment-and-payroll-services")]
+        public IActionResult Recruitment()
+        {
+            return View("~/Views/Home/Recruitment.cshtml");
+        }
+
+        [Route("services/it-development-and-maintenance")]
+        public IActionResult ITdevelopment()
+        {
+            return View("~/Views/Home/ITdevelopment.cshtml");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
