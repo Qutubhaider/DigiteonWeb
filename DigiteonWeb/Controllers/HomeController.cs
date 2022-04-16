d8478d11599d1b1ff47254ed13349c00a7581d7f
qutubhaider950@gmail.com <qutubhaider950@gmail.com>
2022-04-16 22:43:22 +0530

    Add project files.

---
+++ DigiteonWeb/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using DigiteonWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiteonWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+
+        [Route("about-us")]
+        public IActionResult About()
+        {
+            return View("~/Views/Home/About.cshtml");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("~/Views/Home/Privacy.cshtml");
+        }
+
+        [Route("contact-us")]
+        public IActionResult Contact()
+        {
+            return View("~/Views/Home/Contact.cshtml");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
